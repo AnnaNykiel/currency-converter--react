3d806162e4e952237f124a1cbf1cbeee28e44816
Anna Nykiel <nykielanna94@gmail.com>
2023-06-15 15:46:14 +0200

    first commit

---
+++ src/currencies.js
@@ -0,0 +1,13 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: 4.3076,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.6721,
+
+    },
+];
